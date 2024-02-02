00d7bbabb953ac058820aac7acab65c3a6cebb57
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-02-02 22:56:09 +0100

    edit home.php

--- application/home.php
+++ application/home.php
@@ -10,27 +10,46 @@
   </head>
   <body>
     <div class="nav-bar">
-      <a href="home.html"><h1 class="M-logo">JL</h1></a>
+      <a href="home.php"><h1 class="M-logo">JL</h1></a>
       <ul class="list">
-        <li><a href="home.html">Home</a></li>
-        <li><a href="jobs.html">Jobs</a></li>
-        <li><a href="contact.html">Contact</a></li>
-        <li><a href="profile.html">About</a></li>
-        <li><a href="profile.html">Profile</a></li>
-
-        <!-- <select name="options" id="p-options"></select>
-                    <option value="name">Name Surname</option>
-                    <option value="view profile">View profile</option>
-                    <option value="settings">Settings</option>
-                    <option value="privacy">Privacy</option>
-                    <option value="help">Help</option>
-                    <option value="support">Support</option>
-                    <option value="sign out">Sign Out</option>
-                </select> -->
+        <li><a href="home.php">Home</a></li>
+        <li><a href="jobs.php">Jobs</a></li>
+        <li><a href="contact.php">Contact us</a></li>
+        <li><a href="profile.php">About</a></li>
       </ul>
       <div class="reg_log">
-        <li><a href="register.html">Register</a></li>
-        <li><a href="login.html">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="login.php">Login</a></li>
+      </div>
+    </div>
+    <div class="grid">
+      <div class="item">
+        <div id="job-name">job 1</div>
+        <div class="job-C-img">
+          <img src="images/jobs/company-logo.png" alt="img">
+          <p id="job-desc">Job description</p>
+        </div>
+      </div>
+      <div class="item">
+        <div id="job-name">job 1</div>
+        <div class="job-C-img">
+          <img src="images/jobs/company-logo.png" alt="img">
+          <p id="job-desc">Job description</p>
+        </div>
+      </div>
+      <div class="item">
+        <div id="job-name">job 1</div>
+        <div class="job-C-img">
+          <img src="images/jobs/company-logo.png" alt="img">
+          <p id="job-desc">Job description</p>
+        </div>
+      </div>
+      <div class="item">
+        <div id="job-name">job 1</div>
+        <div class="job-C-img">
+          <img src="images/jobs/company-logo.png" alt="img">
+          <p id="job-desc">Job description</p>
+        </div>
       </div>
     </div>
   </body>
