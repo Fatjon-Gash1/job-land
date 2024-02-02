863bd422e677d3e8973863837b1ed339ae9ffa44
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-02-03 00:32:34 +0100

    Edit home.php

--- application/home.php
+++ application/home.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
+
     <script src="header.js" defer></script>
     
     <title>Home | JobLand</title>
@@ -15,42 +18,51 @@
         <li><a href="home.php">Home</a></li>
         <li><a href="jobs.php">Jobs</a></li>
         <li><a href="contact.php">Contact us</a></li>
-        <li><a href="profile.php">About</a></li>
+        <li><a href="about.php">About</a></li>
       </ul>
       <div class="reg_log">
         <li><a href="register.php">Register</a></li>
         <li><a href="login.php">Login</a></li>
       </div>
     </div>
-    <div class="grid">
-      <div class="item">
-        <div id="job-name">job 1</div>
-        <div class="job-C-img">
-          <img src="images/jobs/company-logo.png" alt="img">
-          <p id="job-desc">Job description</p>
+    <section class="section-0">
+        <div class="slider">
+            <div><img src="images/jobs/cocacola.jpg" alt="Image 1"></div>
+            <div><img src="images/jobs/8867.Microsoft_5F00_Logo_2D00_for_2D00_screen-1024x376.jpg" alt="Image 2"></div>
+            <div><img src="images/jobs/nike.jpg" alt="Image 3"></div>
         </div>
-      </div>
-      <div class="item">
-        <div id="job-name">job 1</div>
-        <div class="job-C-img">
-          <img src="images/jobs/company-logo.png" alt="img">
-          <p id="job-desc">Job description</p>
-        </div>
-      </div>
-      <div class="item">
-        <div id="job-name">job 1</div>
-        <div class="job-C-img">
-          <img src="images/jobs/company-logo.png" alt="img">
-          <p id="job-desc">Job description</p>
-        </div>
-      </div>
-      <div class="item">
-        <div id="job-name">job 1</div>
-        <div class="job-C-img">
-          <img src="images/jobs/company-logo.png" alt="img">
-          <p id="job-desc">Job description</p>
-        </div>
-      </div>
-    </div>
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('.slider').slick({
+                autoplay: true,
+                autoplaySpeed: 2000,
+                dots: true,
+                arrows: false
+            });
+        });
+    </script>
+    <section class="section-1">
+      <form action="search_results.php" method="get">
+            <input class="input-search" type="text" id="jobTitle" name="jobTitle" placeholder="Enter job title or keyword" required>
+
+            <select class="select-search" id="jobCategory" name="jobCategory">
+                <option value="" selected disabled>Select category</option>
+                <option value="it">IT</option>
+                <option value="finance">Finance</option>
+                <option value="marketing">Marketing</option>
+                <option value="engineering">Engineering</option>
+                <!-- Add more categories as needed -->
+            </select>
+
+            <button type="submit">Search</button>
+        </form>
+      </section>
   </body>
 </html>
+<?php
+  include('footer.php');
+?>
