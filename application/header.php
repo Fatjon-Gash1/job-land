a5d2149bd6a1905141629991354b16f2a86928c8
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-02-03 21:41:23 +0100

    create header.php

---
+++ application/header.php
@@ -0,0 +1,19 @@
+   <div class="nav-bar">
+    <a href="home.php">
+      <h1 class="M-logo">JL</h1>
+    </a>
+    <div class="dropdown">
+      <button class="dropbtn"><i class="fa fa-bars"></i></button>
+      <ul class="list">
+        <li><a href="home.php">Home</a></li>
+        <li><a href="jobs.php">Jobs</a></li>
+        <li><a href="contact.php">Contact us</a></li>
+        <li><a href="news.php">News</a></li>
+        <li><a href="about.php">About</a></li>
+      </ul>
+    </div>
+    <div class="reg_log">
+      <li><a href="register.php">Register</a></li>
+      <li><a href="login.php">Login</a></li>
+    </div>
+  </div>
