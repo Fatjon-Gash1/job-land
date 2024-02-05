3ec5f55b2b7354139c1e4adab0812b55548b7319
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-02-05 02:43:48 +0100

    Edit contact.php

--- application/contact.php
+++ application/contact.php
@@ -1,3 +1,9 @@
+<?php
+include('database.php');
+
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,36 +17,41 @@
 
 <body>
   <?php
-  include 'header.php';
+  include('header.php');
   ?>
+
   <div class="job-nav"></div>
+
   <h1 id="contact-h1">Contact Us</h1>
   <p id="contact-p">Are you ready to take the next step in your career or find the perfect candidate for your company?<br>
     Our dedicated team at [Your Company Name] is here to assist you. <br>
     Whether you're a job seeker looking for exciting opportunities or an employer seeking top-tier talent,
     we're committed to providing exceptional service.</p>
-  <form class="contact-form" action="submit_form.php" method="post">
-    <label class="nameCT" for="name">Name:</label><br>
-    <input class="inputCT" type="text" id="name" name="name" required><br>
+  <div class="Cform-div">
+    <form class="contact-form" action="submit_form.php" method="post">
+      <label class="nameCT" for="name">Name:</label><br>
+      <input class="inputCT" placeholder="Enter your name" type="text" id="name" name="name" required><br>
 
-    <label class="emailCT" for="email">Email:</label><br>
-    <input class="inputEM" type="email" id="email" name="email" required><br>
+      <label class="emailCT" for="email">Email:</label><br>
+      <input class="inputEM" placeholder="name@example.com" type="email" id="email" name="email" required><br>
 
-    <label class="messageCT" for="message">Message:</label><br>
-    <textarea class="inputMS" id="message" name="message" rows="4" required></textarea><br>
+      <label class="messageCT" for="message">Message:</label><br>
+      <textarea class="inputMS" placeholder="Enter your message" id="message" name="message" rows="4" required></textarea><br>
 
-    <button id="btnCT" type="submit">Send message</button><br>
-  </form>
+      <button id="btnCT" type="submit">Send message</button><br>
+    </form>
+  </div>
   <h2 id="office-h2">Office Hours:</h2>
   <p id="endingC-p">Monday to Friday: 9:00 AM - 5:00 PM<br>
-  Our team of experts is eager to address any inquiries, assist with job postings, or guide you through our platform. <br>
+    Our team of experts is eager to address any inquiries, assist with job postings, or guide you through our platform. <br>
     At [Your Company Name], we believe in connecting people with meaningful opportunities,<br>
     and we look forward to being part of your journey.
   </p><br>
   <p id="lastC-p"><b>Join us in shaping the future of careers! We're here to help you succeed.</b></p>
 </body>
 
-</html>
 <?php
 include 'footer.php';
 ?>
+
+</html>
