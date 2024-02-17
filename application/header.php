bd236c655da409036325cfee39093302d3eaefdc
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-02-17 22:44:00 +0100

    Edit header.php

--- application/header.php
+++ application/header.php
@@ -1,3 +1,14 @@
+<?php
+include("database.php");
+
+session_start();
+if (isset($_SESSION['username'])) {
+    $isLoggedIn = true;
+} else {
+    $isLoggedIn = false;
+}
+?>
+
 <div class="M-logo">
   <h1><a href="home.php">JL</a></h1>
 </div>
@@ -5,14 +16,14 @@
   <button id="button-menu" class="dropbtn"><i class="fa fa-bars"></i></button>
   
   <div class="sidebar">
-  <ul>
-    <li><a href="home.php">Home</a></li>
-    <li><a href="jobs.php">Jobs</a></li>
-    <li><a href="contact.php">Contact us</a></li>
-    <li><a href="news.php">News</a></li>
-    <li><a href="about.php">About</a></li>
-  </ul>
-</div>
+    <ul>
+      <li><a href="home.php">Home</a></li>
+      <li><a href="jobs.php">Jobs</a></li>
+      <li><a href="contact.php">Contact us</a></li>
+      <li><a href="news.php">News</a></li>
+      <li><a href="about.php">About</a></li>
+    </ul>
+  </div>
 
   <ul class="list">
     <li><a href="home.php">Home</a></li>
@@ -23,9 +34,16 @@
   </ul>
 </div>
 <div class="reg_log">
-  <li><a href="register.php">Register</a></li>
-  <li><a href="login.php">Login</a></li>
-  <li><button id="logoutButton" onclick="logout()">Logout</button></li>
+  <?php
+  if ($isLoggedIn) {
+      // User is logged in, show logout button
+      echo '<li><button id="logoutButton" onclick="logout()">Logout</button></li>';
+  } else {
+      // User is not logged in, show register and login buttons
+      echo '<li><a href="register.php">Register</a></li>';
+      echo '<li><a href="login.php">Login</a></li>';
+  }
+  ?>
 </div>
 
 <script>
@@ -42,3 +60,4 @@
     });
   });
 </script>
+
