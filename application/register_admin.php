0f829db246b4e3f2ce572e45b92a4631b836e223
Fatjon-Gash1 <fatjon.gashi.891@gmail.com>
2024-02-25 22:56:55 +0100

    Enable dashboard redirect on admin register

--- application/register_admin.php
+++ application/register_admin.php
@@ -1,4 +1,3 @@
-
 <?php
 include("database.php");
 ?>
@@ -12,19 +11,11 @@ include("database.php");
   <link rel="stylesheet" href="styles.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
   <script src="header.js" defer></script>
-  <script src="script.js" defer></script>
 
   <title>Admin Registration | JobLand</title>
 </head>
 
 <body style="background-color: #ff4a4a">
-
-  <?php
-  include('header.php');
-  ?>
-
-  <div class="job-nav"></div>
-
   <div class="rform-div">
     <form class="rform" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
       <div class="register-header">
@@ -32,7 +23,54 @@ include("database.php");
       </div>
       <input type="text" placeholder="Username" name="username" id="usernameR"><br><br>
       <input type="password" placeholder="Password" name="password" id="passwordR" minlength="8" maxlength="12" /><br><br>
-      <input class="submit_reg" type="submit" name="submit" value="Agree & Register" id="submit">
+      <input style="margin-top: 1em;" class="submit_reg" type="submit" name="submit" value="Agree & Register" id="submit">
+
+      <?php
+
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if (empty($username) || empty($password)) {
+          echo "<p class='error'>Please fill in all the required fields!</p>";
+          echo "<script>
+       let userbox = document.getElementById('usernameR');
+       let passbox = document.getElementById('passwordR');
+       
+       userbox.placeholder = 'Username is required!';
+       userbox.style.backgroundColor = '#f25252';
+
+       userbox.addEventListener('input', function() {
+        userbox.style.backgroundColor = '#ffffff';
+       });
+
+       passbox.placeholder = 'Password is required!';
+       passbox.style.backgroundColor = '#f25252';
+
+       passbox.addEventListener('input', function() {
+        passbox.style.backgroundColor = '#ffffff';
+       });
+       </script>";
+        } else {
+          $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+          $sql = "INSERT INTO admins (username, password) 
+                VALUES ('$username', '$hashed_password')";
+
+          try {
+            mysqli_query($conn, $sql);
+
+            session_start();
+            $_SESSION['username'] = $username;
+            header("Location: dashboard.php");
+          } catch (mysqli_sql_exception) {
+            echo "<p class='error'>That Admin username is already taken!</p>";
+          }
+        }
+      }
+      mysqli_close($conn);
+      ?>
+
       <p>By clicking Agree & Register, <br> you agree to the JobLand <span>User Agreement</span>, <span>Privacy Policy</span>, <br> and <span>Cookie Policy</span>.</p>
 
       <p>Existing account? <a href="admin_login.php"><span id="rp-login">Login</span></a></p>
@@ -43,28 +81,5 @@ include("database.php");
 <?php
 include('footer.php');
 ?>
+
 </html>
-<?php
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-  $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
-  $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
-
-  if (empty($username) || empty($password)) {
-    echo "<p class='error'>Please fill in all fields</p>";
-  } else {
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-    $sql = "INSERT INTO admins (username, password) 
-                VALUES ('$username', '$hashed_password')";
-
-    try {
-      mysqli_query($conn, $sql);
-      echo "<p class='success'>You have registered successfully as a new Admin!</p>";
-    } catch (mysqli_sql_exception) {
-      echo "<p class='error'>That Admin username is already taken</p>";
-    }
-  }
-}
-mysqli_close($conn);
-?>
